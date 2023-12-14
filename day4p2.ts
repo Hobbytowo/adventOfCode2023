5fa7a4ebbd4de3aba38e7ecb5f4d5138a77f8404
Weronika Kędziora <weronika.kedziora@edgeteq.com>
2023-12-14 21:05:41 +0100

    feat: day4p2

--- day4p2.ts
+++ day4p2.ts
@@ -11,11 +11,11 @@ function readModuleFile(path, callback) {
 
 readModuleFile('./day4.txt', function (err, data) {
     const arr = data.toString().split("\n")
+    arr.pop() //rm empty line
 
-    const result = arr.reduce((sum, line) => {
-        if (!line) return sum
-
-        const cardData = line.split(':')[1]
+    // arr of indexes cards to win
+    const copiesToWinPerCard = arr.map((card, idx) => {
+        const cardData = card.split(':')[1]
         const winningNumbers = cardData.split('|')[0].split(' ').filter(x => x)
         const testNumbers = cardData.split('|')[1].split(' ').filter(x => x)
 
@@ -26,11 +26,14 @@ readModuleFile('./day4.txt', function (err, data) {
             }
         })
 
-        const valToAdd = wonNumbers ? 2**(wonNumbers - 1) : 0
-        console.log({winningNumbers, testNumbers, wonNumbers, valToAdd})
+        return Array.from({length: wonNumbers}, (_, i) => i + 2 + idx)
+    })
 
-        return sum + valToAdd
-    }, 0)
+    for (let step = 0; step < copiesToWinPerCard.length; step++) {
+        copiesToWinPerCard[step].forEach(cardNr => {
+            copiesToWinPerCard.push(copiesToWinPerCard[cardNr - 1])
+        })
+    }
 
-    console.log({result})
+    console.log(copiesToWinPerCard.length)
 });
