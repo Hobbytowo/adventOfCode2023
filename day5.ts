abed8a2c89f92673564481d9f502a1d586e746d4
Weronika Kędziora <weronika.kedziora@edgeteq.com>
2023-12-15 09:36:49 +0100

    parseMapping

--- day5.ts
+++ day5.ts
@@ -16,35 +16,45 @@ const groupBy = (arr, number) => {
 }
 
 readModuleFile('./day5.txt', function (err, data) {
-    const parsedData = data.toString().split("\n").filter(x => x).join(" ").split(":").map(x => x.match(/\d+/g)).filter(x => x);
+    const parsedData = data.toString()
+        .split("\n")
+        .filter(x => x)
+        .join(" ")
+        .split(":")
+        .map(x => x.match(/\d+/g))
+        .filter(x => x)
+
+    const parseMapping = arr => arr.map((item) => ({
+        min: item[1]*1,
+        max: item[1]*1 + item[2]*1 - 1,
+        diff: item[0] - item[1]
+    }))
 
     const seeds = parsedData[0]
-    const seedToSoilMap = groupBy(parsedData[1], 3)
-    const soilToFertilizerMap = groupBy(parsedData[2], 3)
-    const fertilizerToWaterMap = groupBy(parsedData[3], 3)
-    const waterToLightMap = groupBy(parsedData[4], 3)
-    const lightToTemperatureMap = groupBy(parsedData[5], 3)
-    const temperatureToHumidityMap = groupBy(parsedData[6], 3)
-    const humidityToLocationMap = groupBy(parsedData[7], 3)
+    const seedToSoilMap = parseMapping(groupBy(parsedData[1], 3))
+    const soilToFertilizerMap = parseMapping(groupBy(parsedData[2], 3))
+    const fertilizerToWaterMap = parseMapping(groupBy(parsedData[3], 3))
+    const waterToLightMap = parseMapping(groupBy(parsedData[4], 3))
+    const lightToTemperatureMap = parseMapping(groupBy(parsedData[5], 3))
+    const temperatureToHumidityMap = parseMapping(groupBy(parsedData[6], 3))
+    const humidityToLocationMap = parseMapping(groupBy(parsedData[7], 3))
+
+    const groupedSeeds = groupBy(seeds, 2)
+    const seedsV2 = groupedSeeds.map(s => Array.from({length: s[1]}, ((_, idx)=> Number(s[0]) + idx))).flat()
 
     const mapFn = (arr, mapArr) => {
         return arr.map(x => {
-            let val = Number(x)
+            let val = x
             mapArr.forEach(map => {
-                const min = Number(map[1])
-                const max = Number(map[1]) + Number(map[2]) - 1
-
-                if (x >= min && x<= max) {
-                    val = Number(x) + (map[0] - map[1])
+                if (x >= map.min && x<= map.max) {
+                    val = x + map.diff
+                    return
                 }
             })
 
             return val
         })
     }
-
-    const groupedSeeds = groupBy(seeds, 2)
-    const seedsV2 = groupedSeeds.map(s => Array.from({length: s[1]}, ((_, idx)=> Number(s[0]) + idx))).flat()
 
     const soil = mapFn(seedsV2, seedToSoilMap)
     const fertilizer = mapFn(soil, soilToFertilizerMap)
